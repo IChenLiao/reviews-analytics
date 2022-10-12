3ed6f0a31c3412e50dc70a7c4b6d5cb491627c64
Ichen Liao <tlvfe52613@gmail.com>
2022-10-12 23:04:37 +0800

    add word count function

--- read.py
+++ read.py
@@ -1,40 +1,37 @@
 data = []
-count = 0 #記數用
-with open ('reviews.txt', 'r') as f:
+count = 0
+with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0: # %是用來求餘數
-			print(len(data)) #每讀到1000筆資料就印出進度出來，print很花時間
+		if count % 1000 == 0:
+			print(len(data))
+print('檔案讀取完了，總共有', len(data), '筆資料')
 
-print('檔案讀取完了，總共有', len(data), '筆資料') #直接印出總數出來
+# print(data[0])
 
-#寫程式碼算出留言的平均長度
-
-sum_len = 0
+wc = {} #word count
 for d in data:
-	sum_len = sum_len + len(d)
+	words = d.split() #split預設值是用空白符號切
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 #新增新的key進wc字典
 
-print('留言平均長度是', sum_len/len(data))
+for word in wc:
+	if wc[word] > 1000000 :
+		print(word, wc[word])
+print(len(wc))
+print(wc['Tina'])
 
-new = []
-for d in data:
-	if len(d) < 100:
-		new.append(d)
-print('一共有', len(new), '筆留言長度小於100')
-print(new[0])
+while True:
+	word = input('請問你想查什麼字: ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現過的次數為: ', wc[word])
+	else:
+		print('這個字沒有出現過哦!')
 
-
-good = []
-for d in data:
-	if 'good' in d:
-		good.append(d)
-print('一共有', len(good), '筆留言提到good')
-print(good[0])
-
-#list conprehension 清單快寫法
-good = [d for d in data if 'good' in d]
-#print(good)
-
-bad = ['bad' in d for d in data]
-#print(bad)
+print('感謝使用本查詢功能')
